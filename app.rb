d08791cadd5edfe02254be0dbb96e82c6f5b2f35
btwashburn <bruce_washburn@oclc.org>
2015-04-30 14:59:15 -0700

    direct creates and updates to status

--- app.rb
+++ app.rb
@@ -32,11 +32,9 @@ end
 
 get '/detect/:term' do
   detect_unicode_block(params[:term])
-  #detect_script(params[:term])
 end
 
 get '/record/new' do
-  # puts ; puts "get /record/new " + request.path_info; puts
   haml :new, :layout => :template
 end
 
@@ -46,10 +44,9 @@ post '/record/create' do
   record = marc_record_from_params('',params)
   @bib = Bib.new_from_marc(record, session[:token])
   @bib.create
-  # puts ; puts "get /record/create @bib.response_code is " + @bib.response_code; puts
   if @bib.response_code == '201'
-    # puts ; puts "get /record/create redirect to url /record/#{@bib.marc_record['001'].value.gsub(/\D/, '')}"; puts
-    redirect url("/record/#{@bib.marc_record['001'].value.gsub(/\D/, '')}")
+    #redirect url("/record/#{@bib.marc_record['001'].value.gsub(/\D/, '')}")
+    redirect url("/status/created/#{@bib.marc_record['001'].value.gsub(/\D/, '')}")
   else
     # puts ; puts "get /record/create apply new layout"; puts
     haml :new, :layout => :template
@@ -57,50 +54,51 @@ post '/record/create' do
 end
 
 get '/record/:oclc_number.?:format?' do
-  # puts ; puts "get /record/:oclc_number.?:format? with oclc number " + params[:oclc_number]; puts
   @bib = Bib.new(params[:oclc_number], session[:token])
-  # puts ; puts "get /record/:oclc_number.?:format? @bib.response_code is " + @bib.response_code; puts
   if @bib.response_code == '200' or @bib.response_code == '201'
     if params[:format] == 'xml'
-      # puts ; puts "get /record/:oclc_number.?:format? format xml"; puts
       content_type :xml
       @bib.marc_record.to_xml.to_s
     elsif params[:format] == 'mrc'
-      # puts ; puts "get /record/:oclc_number.?:format? format mrc"; puts
       content_type 'application/marc'
       @bib.marc_record.to_marc
     else
-      # puts ; puts "get /record/:oclc_number.?:format? apply record layout"; puts
       haml :record, :layout => :template
     end
   elsif @bib.response_code == '404'
-    # puts ; puts "get /record/:oclc_number.?:format? apply not_found layout"; puts
     haml :not_found, :layout => :template
   else 
-    # puts ; puts "get /record/:oclc_number.?:format? apply error layout"; puts
     haml :error, :layout => :template
   end
 end
 
 post '/record/update' do 
-  # puts ; puts "get /record/update params[:language]=" + params[:language]; puts
   session[:marc_language] = params[:language]
   @bib = Bib.new(params[:oclc_number], session[:token])
   record = marc_record_from_params(@bib.marc_record, params)
   @bib.marc_record = record
   @bib.update
-  # puts ; puts "get /record/update @bib.response_code is " + @bib.response_code; puts
   if @bib.response_code == '200' or @bib.response_code == '201'
-    # puts ; puts "get /record/update redirect to /record/#{@bib.marc_record['001'].value.gsub(/\D/, '')}"; puts
-    redirect url("/record/#{@bib.marc_record['001'].value.gsub(/\D/, '')}")
+    #redirect url("/record/#{@bib.marc_record['001'].value.gsub(/\D/, '')}")
+    redirect url("/status/updated/#{@bib.marc_record['001'].value.gsub(/\D/, '')}")
   else
-    # puts ; puts "get /record/update apply record layout " + @bib.response_code; puts
     haml :record, :layout => :template
   end
 end
 
+get '/status/:type/:oclc_number' do
+  @bib = Bib.new(params[:oclc_number], session[:token])
+  @type = params[:type]
+  if @bib.response_code == '200' or @bib.response_code == '201'
+    haml :status, :layout => :template
+  elsif @bib.response_code == '404'
+    haml :not_found, :layout => :template
+  else 
+    haml :error, :layout => :template
+  end
+end
+
 get '/logoff' do
-  # puts ; puts "get /logoff"; puts
   session[:token] = nil
   redirect url("/")
 end
@@ -110,13 +108,11 @@ get '/catch_auth_code' do
     session[:token] = WSKEY.auth_code_token(params[:code], session[:registry_id], session[:registry_id])
     redirect session[:path]
   else
-    # If the user's session is gone, go to the home page and have them reselect the authenticating inst ID.
     redirect url('/')
   end
 end
 
 get '/authenticate' do
-  # puts ; puts "authenticating" ; puts
   authenticate
   redirect session[:path]
 end
@@ -130,14 +126,11 @@ error do
 end
 
 def authenticate
-  # puts ; puts "authenticate method" ; puts
   session[:registry_id] = params[:registry_id] if params[:registry_id] 
   if session[:token].nil?
-    # puts ; puts "session[:token] is nil" ; puts
     login_url = WSKEY.login_url(session[:registry_id], session[:registry_id])
     redirect login_url
   elsif session[:token].expired?
-    # puts ; puts "session[:token].expired" ; puts
     login_url = WSKEY.login_url(session[:registry_id], session[:registry_id])
     redirect login_url
   end
